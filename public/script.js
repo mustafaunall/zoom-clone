c55bc4fab2d1a95c3399879e07d9f62ebef3db9a
Mustafa Ünal <revengefiretr@gmail.com>
2020-07-26 19:31:17 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
-const socket = io.connect('212.154.2.202:80')
+const socket = io()
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: '212.154.2.202',
+    host: '/',
     port: 3001
 })
 const videom = document.createElement('video')
