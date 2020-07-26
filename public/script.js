50da5330876ababcf4f334435957f89d3796745c
Mustafa Ünal <revengefiretr@gmail.com>
2020-07-26 23:34:41 +0300

    worth

--- public/script.js
+++ public/script.js
@@ -1,8 +1,6 @@
 const socket = io()
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: '/',
-    port: 3001
 })
 const videom = document.createElement('video')
 videom.muted = true
